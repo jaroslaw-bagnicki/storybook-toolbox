263d23a27cf030b4fecae7868b36a4d2aa0dc5e4
Jarosław Bagnicki <jarek@bagnicki.net>
2019-11-24 17:49:36 +0100

    Fix styles and some cleanup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import '../src/index.css';
 
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../src/stories', true, /\.stories\.tsx?$/), module);
