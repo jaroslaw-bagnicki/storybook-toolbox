151d40e6f7623ffe1be5e0f6b194dd40a57eb7d4
Jarosław Bagnicki <jarek@bagnicki.net>
2019-11-24 18:04:58 +0100

    Finish Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { TaskModel } from '../models';
+import cls from 'classnames';
 
 type Props = {
     task: TaskModel,
@@ -8,8 +9,27 @@ type Props = {
 }
 
 const Task: React.FunctionComponent<Props> = ({ task: { id, title, state }, onArchiveTask, onPinTask}) => (
-    <div className="list-item">
-        <input type="text" value={title} readOnly={true} />
+    <div className={cls('list-item', state)}>
+        <label className="checkbox">
+            <input 
+                name="checked"
+                type="checkbox"
+                defaultChecked={state === 'TASK_ARCHIVED'}
+                disabled />
+            <span className="checkbox-custom" onClick={_ => onArchiveTask(id)} />
+        </label>
+
+        <div className="title">
+            <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        </div>
+
+        <div className="actions" onClick={ev => ev.stopPropagation()}>
+            { state !== 'TASK_ARCHIVED' &&
+                <a onClick={_ => onPinTask(id)}>
+                    <span className="icon-star" />
+                </a>
+            }
+        </div>
     </div>
 );
 
