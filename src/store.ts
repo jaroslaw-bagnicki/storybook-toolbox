d6e29ea66b1ddfc599c24b4b76f5bffda923a5c1
Jarosław Bagnicki <jarek@bagnicki.net>
2019-12-08 10:42:48 +0100

    Create redux store

---
+++ src/store.ts
@@ -0,0 +1,43 @@
+import { createStore, combineReducers } from 'redux'
+import { action, ActionType, StateType } from 'typesafe-actions'
+import { TaskModel } from './models'
+
+const ARCHIVE_TASK = 'ARCHIVE_TASK'
+const PIN_TASK = 'PIN_TASK'
+
+export const archiveTask = (id: string) => action(ARCHIVE_TASK, { id })
+export const pinTask = (id: string) => action(PIN_TASK, { id })
+
+type TaskActions = ActionType<typeof archiveTask | typeof pinTask>
+
+export const tasksReducer = (state: TaskModel[] = [], { type, payload }: TaskActions): TaskModel[] => {
+    switch (type) {
+        case ARCHIVE_TASK:
+            return state.map(task => task.id === payload.id ? { ...task, state: 'TASK_ARCHIVED' } : task)
+
+        case PIN_TASK:
+        return state.map(task => task.id === payload.id ? { ...task, state: 'TASK_PINNED' } : task)
+
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+})
+
+export type TodosState = StateType<typeof rootReducer>;
+
+const initalState: TodosState = {
+    tasks: [
+        { id: '1', title: 'Something', state: 'TASK_INBOX', updateAt: new Date('2018-01-01T09:00Z') },
+        { id: '2', title: 'Something more', state: 'TASK_INBOX', updateAt: new Date('2018-01-01T09:00Z') },
+        { id: '3', title: 'Something else', state: 'TASK_INBOX', updateAt: new Date('2018-01-01T09:00Z') },
+        { id: '4', title: 'Something again', state: 'TASK_INBOX', updateAt: new Date('2018-01-01T09:00Z') },
+    ],
+};
+
+const store = createStore(rootReducer, initalState);
+
+export default store;
